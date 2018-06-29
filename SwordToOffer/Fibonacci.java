498cd0704ca84d1faabdfb24931ccef4fabeaf64
acba <412862016@qq.com>
2018-06-29 17:16:50 +0800

    add problem 7,8 to SwordToOffer

---
+++ SwordToOffer/Fibonacci.java
@@ -0,0 +1,25 @@
+/* 大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项。
+n<=39 */
+
+public class Solution {
+    public int Fibonacci(int n) {
+        
+        if (n<=0) {
+            return 0;
+        }
+        
+        if (n==1 || n==2){
+            return 1;
+        }
+        
+        int[] fibonacci = new int[n];
+        fibonacci[0] = 1;
+        fibonacci[1] = 1;
+        
+        for (int i = 2; i < n; i++) {
+            fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+        }
+        
+        return fibonacci[n-1];
+    }
+}
---
+++ SwordToOffer/JumpFloors.java
@@ -0,0 +1,26 @@
+/* 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法。*/
+
+public class Solution {
+    public int JumpFloor(int target) {
+        
+        if (target <= 0) {
+            return 0;
+        }
+        if (target == 1){
+            return 1;
+        }
+        if (target == 2) {
+            return 2;
+        }
+        
+        int[] methods = new int[target];
+        methods[0] = 1;
+        methods[1] = 2;
+        
+        for (int i = 2; i < target; i++) {
+            methods[i] = methods[i-1]+methods[i-2];
+        }
+        
+        return methods[target-1];
+    }
+}
