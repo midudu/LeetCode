44bc3c0c84e04a0a5eb414d50e5b07412dcfe9a3
acba <412862016@qq.com>
2018-07-06 20:50:12 +0800

    add 17,18 to SwordToOffer

--- SwordToOffer/JudgeIfSubTree.java
+++ SwordToOffer/JudgeIfSubTree.java
@@ -1,4 +1,7 @@
-class TreeNode {
+/* 输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构） */
+
+/**
+public class TreeNode {
     int val = 0;
     TreeNode left = null;
     TreeNode right = null;
@@ -7,48 +10,44 @@ class TreeNode {
         this.val = val;
 
     }
+
 }
-
+*/
 public class Solution {
-    public boolean HasSubtree(TreeNode root1, TreeNode root2) {
-
-        if (root1 == null || root2 == null) {
+    public boolean HasSubtree(TreeNode root1,TreeNode root2) {
+        
+        if ( root1 == null || root2 == null ) {
             return false;
         }
-
+        
         boolean result = false;
         if (root1.val == root2.val) {
             result = ifTwoTreesEqual(root1, root2);
         }
-
+        
         if (!result) {
             result = HasSubtree(root1.left, root2);
         }
-
+        
         if (!result) {
             result = HasSubtree(root1.right, root2);
         }
-
+        
         return result;
     }
-
+    
     private boolean ifTwoTreesEqual(TreeNode root1, TreeNode root2) {
-
-        if (root1 == null || root2 == null) {
-            return root1 == root2;
+        
+        if (root2 == null) {
+            return true;
         }
-
+        
+        if (root1 == null) {
+            return false;
+        }
+        
         return root1.val == root2.val
-                && ifTwoTreesEqual(root1.left, root2.left)
-                && ifTwoTreesEqual(root1.right, root2.right);
-    }
-
-    public static void main(String[] args) {
-
-        TreeNode root1 = new TreeNode(8);
-        root1.left = new TreeNode(8);
-        root1.right = new TreeNode(7);
-        root1.left.left = new TreeNode(9);
-        root1.left.right = new TreeNode(2);
+            && ifTwoTreesEqual(root1.left, root2.left)
+            && ifTwoTreesEqual(root1.right, root2.right);
     }
 }
---
+++ SwordToOffer/MirrorOfBinaryTree.java
@@ -0,0 +1,60 @@
+/* 题目描述
+操作给定的二叉树，将其变换为源二叉树的镜像。
+输入描述:
+二叉树的镜像定义：源二叉树 
+    	    8
+    	   /  \
+    	  6   10
+    	 / \  / \
+    	5  7 9 11
+    	镜像二叉树
+    	    8
+    	   /  \
+    	  10   6
+    	 / \  / \
+    	11 9 7  5 */
+		
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public void Mirror(TreeNode root) {
+        
+        if (root == null) {
+            return;
+        }
+        
+        MirrorHelper(root);
+    }
+    
+    private void MirrorHelper(TreeNode root) {
+        
+        if (root.left == null && root.right == null) {
+            return;
+        } else if (root.left == null && root.right != null) {
+            MirrorHelper(root.right);
+            root.left = root.right;
+            root.right = null;
+        } else if (root.left != null && root.right == null) {
+            MirrorHelper(root.left);
+            root.right = root.left;
+            root.left = null;
+        } else {
+            MirrorHelper(root.left);
+            MirrorHelper(root.right);
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+        }
+    }
+}
