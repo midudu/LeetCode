cbeaece8dea4a9ad86d9fd3b97954b6adcd539d0
acba <412862016@qq.com>
2018-07-12 17:30:37 +0800

    add 23 to SwordToOffer

---
+++ SwordToOffer/FindPathOfSpecificSum.java
@@ -0,0 +1,21 @@
+/* 输入一颗二叉树的跟节点和一个整数，打印出二叉树中结点值的和为输入整数的所有路径。路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。(注意: 在返回值的list中，数组长度大的数组靠前) */
+
+import java.util.ArrayList;
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root,int target) {
+        
+    }
+}
---
+++ SwordToOffer/JudgePostOrderOfBST.java
@@ -0,0 +1,41 @@
+/* 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。
+如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。 */
+
+public class Solution {
+    public boolean VerifySquenceOfBST(int [] sequence) {
+        
+        if ( sequence == null || sequence.length == 0 ) {
+			return false;
+		}
+		
+		return VerifySquenceOfBSTHelper(sequence, 0, sequence.length - 1);
+    }
+	
+	private boolean VerifySquenceOfBSTHelper(int[] sequence, int startIndex, int endIndex) {
+		
+		if ( startIndex >= endIndex ) {
+			return true;
+		}
+		
+		int rootValue = sequence[endIndex];
+		
+		int i = 0;
+		for (i = startIndex; i < endIndex; i++) {
+			
+			if (sequence[i] > rootValue) {
+				break;
+			}
+		}
+		
+		int newStartIndex = i;
+		for (; i < endIndex; i++) {
+			
+			if (sequence[i] < rootValue) {
+				return false;
+			}
+		}
+		
+		return VerifySquenceOfBSTHelper(sequence, startIndex, newStartIndex - 1) 
+		    && VerifySquenceOfBSTHelper(sequence, newStartIndex, endIndex - 1);
+	}
+}
