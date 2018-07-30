b5c36c708d3273b59ee5daad1e1a725f714ab02c
Bo Chen <412862016@qq.com>
2018-07-30 15:56:20 +0800

    add 39 to SwordToOffer

--- Java_Version/TestJava/src/Solution.java
+++ Java_Version/TestJava/src/Solution.java
@@ -1,87 +1,56 @@
+import java.util.HashMap;
+
+class TreeNode {
+
+    TreeNode left;
+    TreeNode right;
+}
+
 public class Solution {
 
-    public static void main(String[] args) {
+    public boolean IsBalanced_Solution(TreeNode root) {
 
-        int[] array = {3,3,3,3,4,5};
+        if (root == null) {
+            return true;
+        }
 
-        int result = new Solution().GetNumberOfK(array, 3);
+        return isBalancedSolutionHelper(root);
     }
 
-    public int GetNumberOfK(int[] array, int k) {
+    private boolean isBalancedSolutionHelper(TreeNode root) {
 
-        if (array == null || array.length == 0) {
+        if (root.left == null && root.right == null) {
+            return true;
+
+        } else if (root.left != null && root.right == null) {
+            return getHeight(root.left) <= 1;
+
+        } else if (root.left == null && root.right != null) {
+            return getHeight(root.right) <= 1;
+
+        } else {
+
+            int leftHeight = getHeight(root.left);
+            int rightHeight = getHeight(root.right);
+
+            return isBalancedSolutionHelper(root.left) && isBalancedSolutionHelper(root.right)
+                    && Math.abs(leftHeight - rightHeight) <= 1;
+        }
+    }
+
+    private HashMap<TreeNode, Integer> height = new HashMap<TreeNode, Integer>();
+
+    private int getHeight(TreeNode root) {
+
+        if (root == null) {
             return 0;
         }
 
-        int k_index = findK(array, k);
-        if (k_index == -1) {
-            return 0;
+        if (height.containsKey(root)) {
+            return height.get(root);
+        } else {
+
+            return 1 + Math.max(getHeight(root.left), getHeight(root.right));
         }
-
-        int first_less_k_index = findFirstLessThanKIndex(array, 0, k_index, k);
-
-        int first_more_k_index = findFirstMoreThanKIndex(array, k_index, array.length - 1, k);
-        if (first_more_k_index == -1) {
-            first_more_k_index = array.length;
-        }
-
-        return (first_more_k_index - first_less_k_index - 1);
-    }
-
-    private int findK(int[] array, int k) {
-
-        int startIndex = 0;
-        int endIndex = array.length - 1;
-
-        while (startIndex <= endIndex) {
-            int middleIndex = startIndex + (endIndex - startIndex) / 2;
-            if (array[middleIndex] == k) {
-                return middleIndex;
-            } else if (array[middleIndex] < k) {
-                startIndex = middleIndex + 1;
-            } else {
-                endIndex = middleIndex - 1;
-            }
-        }
-
-        return -1;
-    }
-
-    private int findFirstLessThanKIndex(int[] array, int startIndex, int endIndex, int k) {
-
-        if (array[startIndex] == k) {
-            return -1;
-        }
-
-        while (startIndex < endIndex - 1) {
-
-            int middleIndex = startIndex + (endIndex - startIndex) / 2;
-            if (array[middleIndex] < k) {
-                startIndex = middleIndex;
-            } else {
-                endIndex = middleIndex - 1;
-            }
-        }
-
-        return (array[endIndex] == k ? startIndex : endIndex);
-    }
-
-    private int findFirstMoreThanKIndex(int[] array, int startIndex, int endIndex, int k) {
-
-        if (array[endIndex] == k) {
-            return -1;
-        }
-
-        while (startIndex < endIndex - 1) {
-
-            int middleIndex = startIndex + (endIndex - startIndex) / 2;
-            if (array[middleIndex] > k) {
-                endIndex = middleIndex;
-            } else {
-                startIndex = middleIndex + 1;
-            }
-        }
-
-        return (array[startIndex] != k ? startIndex : endIndex);
     }
 }
---
+++ SwordToOffer/39_JudgeIfBalanceBinaryTree.java
@@ -0,0 +1,52 @@
+/* 输入一棵二叉树，判断该二叉树是否是平衡二叉树 */
+
+import java.util.HashMap;
+
+public class Solution {
+
+  public boolean IsBalanced_Solution(TreeNode root) {
+
+    if (root == null) {
+      return true;
+    }
+
+    return isBalancedSolutionHelper(root);
+  }
+
+  private boolean isBalancedSolutionHelper(TreeNode root) {
+
+    if (root.left == null && root.right == null) {
+      return true;
+
+    } else if (root.left != null && root.right == null) {
+      return getHeight(root.left) <= 1;
+
+    } else if (root.left == null && root.right != null) {
+      return getHeight(root.right) <= 1;
+
+    } else {
+
+      int leftHeight = getHeight(root.left);
+      int rightHeight = getHeight(root.right);
+
+      return isBalancedSolutionHelper(root.left) && isBalancedSolutionHelper(root.right)
+        && Math.abs(leftHeight - rightHeight) <= 1;
+    }
+  }
+
+  private HashMap<TreeNode, Integer> height = new HashMap<TreeNode, Integer>();
+
+  private int getHeight(TreeNode root) {
+
+    if (root == null) {
+      return 0;
+    }
+
+    if (height.containsKey(root)) {
+      return height.get(root);
+    } else {
+
+      return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+    }
+  }
+}
