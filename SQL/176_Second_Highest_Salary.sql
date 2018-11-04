a21336c55cfe4f998c37f99a9045e82f292e0b3c
Bo Chen <412862016@qq.com>
2018-11-04 09:48:26 +0800

    add 54, 176, 177 to LeetCode

--- SQL/176_Second_Highest_Salary.sql
+++ SQL/176_Second_Highest_Salary.sql
@@ -15,7 +15,14 @@ For example, given the above Employee table, the query should return 200 as the 
 | 200                 |
 +---------------------+ */
 
-SELECT 
-(SELECT DISTINCT Salary FROM Employee
-ORDER BY Salary DESC LIMIT 1 OFFSET 1) 
-AS SecondHighestSalary;
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+
+  SET N = N-1;
+
+  RETURN (
+      # Write your MySQL query statement below.
+      
+      SELECT (SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1 OFFSET N)
+  );
+END
