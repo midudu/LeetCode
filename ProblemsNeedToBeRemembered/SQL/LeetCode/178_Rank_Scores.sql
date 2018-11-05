1d6adebfad1c4b0dab156714470402e5ef503066
Bo Chen <412862016@qq.com>
2018-11-05 10:14:18 +0800

    add 55, 56, 178 to LeetCode

---
+++ ProblemsNeedToBeRemembered/SQL/LeetCode/178_Rank_Scores.sql
@@ -0,0 +1,32 @@
+/* Write a SQL query to rank scores. If there is a tie between two scores, both should have the same ranking. Note that after a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no "holes" between ranks.
+
++----+-------+
+| Id | Score |
++----+-------+
+| 1  | 3.50  |
+| 2  | 3.65  |
+| 3  | 4.00  |
+| 4  | 3.85  |
+| 5  | 4.00  |
+| 6  | 3.65  |
++----+-------+
+For example, given the above Scores table, your query should generate the following report (order by highest score):
+
++-------+------+
+| Score | Rank |
++-------+------+
+| 4.00  | 1    |
+| 4.00  | 1    |
+| 3.85  | 2    |
+| 3.65  | 3    |
+| 3.65  | 3    |
+| 3.50  | 4    |
++-------+------+ */
+
+/* 有更好的解法，但是看不懂 */
+
+SELECT S1.Score, COUNT(DISTINCT S2.Score) AS Rank FROM 
+Scores AS S1 INNER JOIN Scores AS S2
+ON S2.Score >= S1.Score 
+GROUP BY S1.Id
+ORDER BY Score DESC;
