25721b3670eef3b407528c9104d0f184b4be3140
Bo Chen <412862016@qq.com>
2018-12-03 08:44:10 +0800

    add 93 to LeetCode

---
+++ Java_Version/93_Restore_IP_Addresses.java
@@ -0,0 +1,88 @@
+/*
+Given a string containing only digits,
+restore it by returning all possible valid IP address combinations.
+
+Example:
+
+Input: "25525511135"
+Output: ["255.255.11.135", "255.255.111.35"]*/
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+    public List<String> restoreIpAddresses(String s) {
+
+        if (s == null || s.length() < 4 || s.length() > 12) {
+            return new ArrayList<>();
+        }
+
+        List<String> result = new ArrayList<>();
+
+        List<String> existingResult = new ArrayList<>();
+
+        restoreIpAddressHelper(s, 0, existingResult, result);
+
+        return result;
+    }
+
+    private void restoreIpAddressHelper(
+            String s, int currentIndex,
+            List<String> existingResult, List<String> result) {
+
+        if (existingResult.size() == 4) {
+
+            if (currentIndex >= s.length()) {
+
+                StringBuilder currentIp = new StringBuilder();
+                for (int i = 0; i < 3; i++) {
+                    currentIp.append(existingResult.get(i));
+                    currentIp.append('.');
+                }
+                currentIp.append(existingResult.get(3));
+
+                result.add(currentIp.toString());
+            }
+
+            return;
+        }
+
+        if (currentIndex >= s.length()) {
+            return;
+        }
+
+        // 1 bit
+        existingResult.add(s.substring(currentIndex, currentIndex + 1));
+        restoreIpAddressHelper(s, currentIndex + 1, existingResult, result);
+        existingResult.remove(existingResult.size() - 1);
+
+        // 2 bit
+        if (currentIndex <= s.length() - 2) {
+            String twoBitIp = s.substring(currentIndex, currentIndex + 2);
+            if (Integer.valueOf(twoBitIp) >= 10
+                    && Integer.valueOf(twoBitIp) <= 99) {
+
+                existingResult.add(twoBitIp);
+                restoreIpAddressHelper(s, currentIndex + 2,
+                        existingResult, result);
+                existingResult.remove(existingResult.size() - 1);
+            }
+        }
+
+        // 3 bit
+        if (currentIndex <= s.length() - 3) {
+
+            String threeBitIp = s.substring(currentIndex, currentIndex + 3);
+            if (Integer.valueOf(threeBitIp) >= 100
+                    && Integer.valueOf(threeBitIp) <= 255) {
+
+                existingResult.add(threeBitIp);
+                restoreIpAddressHelper(s, currentIndex + 3,
+                        existingResult, result);
+                existingResult.remove(existingResult.size() - 1);
+            }
+        }
+    }
+}
---
+++ Java_Version/94_Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,84 @@
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+        Example:
+
+        Input: [1,null,2,3]
+        1
+        \
+        2
+        /
+        3
+
+        Output: [1,3,2]
+        Follow up: Recursive solution is trivial, could you do it iteratively?*/
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+// Method 2: Non-Recursive version
+class Solution {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode currentNode = root;
+
+        while(currentNode != null || !stack.isEmpty()) {
+
+            if(currentNode != null) {
+                stack.push(currentNode);
+                currentNode = currentNode.left;
+            } else {
+                currentNode = stack.pop();
+                result.add(currentNode.val);
+                currentNode = currentNode.right;
+            }
+        }
+        return result;
+    }
+}
+
+
+/* Method 1: Recursive version
+class Solution {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        inorderTraversalHelper(root, result);
+
+        return result;
+    }
+
+    private void inorderTraversalHelper(TreeNode root, List<Integer> result) {
+
+        if (root.left != null) {
+            inorderTraversalHelper(root.left, result);
+        }
+
+        result.add(root.val);
+
+        if (root.right != null) {
+            inorderTraversalHelper(root.right, result);
+        }
+    }
+}*/
+
