6a2f94afb43298eba3fdabd5055a54dbe79e165e
Bo Chen <412862016@qq.com>
2018-12-11 15:32:55 +0800

    add 197 to LeetCode

--- Java_Version/TestJava/src/Solution.java
+++ Java_Version/TestJava/src/Solution.java
@@ -1,101 +1,15 @@
-/*
-There are a total of n courses you have to take, labeled from 0 to n-1.
 
-Some courses may have prerequisites,
-for example to take course 0 you have to first take course 1,
-which is expressed as a pair: [0,1]
-
-Given the total number of courses and a list of prerequisite pairs,
-is it possible for you to finish all courses?
-
-        Example 1:
-
-        Input: 2, [[1,0]]
-        Output: true
-        Explanation: There are a total of 2 courses to take.
-        To take course 1 you should have finished course 0. So it is possible.
-        Example 2:
-
-        Input: 2, [[1,0],[0,1]]
-        Output: false
-        Explanation: There are a total of 2 courses to take.
-        To take course 1 you should have finished course 0, and to take course 0 you should
-        also have finished course 1. So it is impossible.
-        Note:
-
-The input prerequisites is a graph represented by a list of edges,
-not adjacency matrices. Read more about how a graph is represented.
-
-You may assume that there are no duplicate edges in the input prerequisites.*/
-
-
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 class Solution {
 
-    public boolean canFinish(int n, int[][] prerequisites) {
+    public static void main(String[] args) {
 
-        if (n <= 0 || prerequisites == null || prerequisites.length == 0) {
-            return true;
-        }
+        List<Integer> list = Arrays.asList(3,4,5);
 
-        List<ArrayList<Integer>> edges = new ArrayList<ArrayList<Integer>>();
-
-        for (int i = 0; i < n; i++) {
-            edges.add(new ArrayList<Integer>());
-        }
-        for (int[] pair : prerequisites) {
-            edges.get(pair[0]).add(pair[1]);
-        }
-
-        return !isCyclic(edges);
-    }
-
-    private boolean isCyclic(List<ArrayList<Integer>> edges) {
-
-        boolean[] hasChecked = new boolean[edges.size()];
-        boolean[] hasVisitedInCurrentPath = new boolean[edges.size()];
-
-        for (int i = 0; i < edges.size(); i++) {
-
-            if (!hasChecked[i]) {
-
-                if (isCyclicHelper(i, edges, hasChecked, hasVisitedInCurrentPath)) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    private boolean isCyclicHelper(
-            int currentNode,
-            List<ArrayList<Integer>> edges,
-            boolean[] hasChecked,
-            boolean[] hasVisitedInCurrentPath) {
-
-        if (hasVisitedInCurrentPath[currentNode]) {
-            return true;
-        }
-
-        if (hasChecked[currentNode]) {
-            return false;
-        }
-
-
-        hasChecked[currentNode] = true;
-        hasVisitedInCurrentPath[currentNode] = true;
-
-        for (Integer edge : edges.get(currentNode)) {
-
-            if (isCyclicHelper(edge, edges, hasChecked, hasVisitedInCurrentPath)) {
-                return true;
-            }
-        }
-
-        hasVisitedInCurrentPath[currentNode] = false;
-        return false;
+        List<Integer> integerList = Collections.synchronizedList(list);
+        integerList.add(3);
     }
 }
---
+++ SQL/197_Rising_Temperature.sql
@@ -0,0 +1,23 @@
+/* Given a Weather table, write a SQL query to find all dates' Ids with higher temperature compared to its previous (yesterday's) dates.
+
++---------+------------------+------------------+
+| Id(INT) | RecordDate(DATE) | Temperature(INT) |
++---------+------------------+------------------+
+|       1 |       2015-01-01 |               10 |
+|       2 |       2015-01-02 |               25 |
+|       3 |       2015-01-03 |               20 |
+|       4 |       2015-01-04 |               30 |
++---------+------------------+------------------+
+For example, return the following Ids for the above Weather table:
+
++----+
+| Id |
++----+
+|  2 |
+|  4 |
++----+ */
+
+SELECT W2.Id FROM Weather AS W1
+INNER JOIN Weather AS W2
+ON TO_DAYS(W2.RecordDate) = TO_DAYS(W1.RecordDate) + 1
+WHERE W2.Temperature > W1.Temperature;
