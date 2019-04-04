b3ef8d341fdb6820911f360a45c3a13d43f91cf2
Bo Chen <412862016@qq.com>
2019-04-04 15:03:38 +0800

    review 83; add 309 of LeetCode

--- Java_Version/83_Remove_Duplicates_from_Sorted_List.java
+++ Java_Version/83_Remove_Duplicates_from_Sorted_List.java
@@ -1,33 +1,10 @@
-/*Author: Bochen (mddboc@foxmail.com)
-Last Modified: Tue Apr 10 22:28:45 CST 2018*/
+/*Given a sorted linked list, delete all duplicates such that each element
+appear only once.
 
-/*Given a sorted linked list, delete all duplicates such that each element appear only once.
-
-        For example,
+For example,
         Given 1->1->2, return 1->2.
-        Given 1->1->2->3->3, return 1->2->3.*/
-
-
-import java.util.*;
-import java.lang.Math;
-import java.lang.System;
-import java.lang.Integer;
-
-
-public class Main {
-
-    public static void main(String[] args) throws ArithmeticException {
-
-        int[] input = {7, 1, 5, 3, 6, 4};
-
-        Solution solution = new Solution();
-
-        int result = solution.maxProfit(input);
-
-        System.out.println("haha");
-    }
-
-}
+        Given 1->1->2->3->3, return 1->2->3.
+*/
 
 
 class ListNode {
@@ -40,29 +17,19 @@ class ListNode {
 }
 
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
-}
-
-
 class Solution {
+
     public ListNode deleteDuplicates(ListNode head) {
 
-        if (head == null){
-            return head;
+        if (head == null) {
+            return null;
         }
 
         ListNode slow = head, fast = head.next;
 
         while (fast != null) {
 
-            if (fast.val != slow.val){
+            if (fast.val != slow.val) {
                 slow.next = fast;
                 slow = slow.next;
             }
