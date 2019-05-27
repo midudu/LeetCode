4c3aa9231d9436aba6c494e79915b4f4927a7fdd
Bo Chen <412862016@qq.com>
2019-05-27 20:20:08 +0800

    review 104; add 376 of LeetCode

--- Java_Version/104_Maximum_Depth_of_Binary_Tree.java
+++ Java_Version/104_Maximum_Depth_of_Binary_Tree.java
@@ -1,53 +1,24 @@
-/*Author: Bochen (mddboc@foxmail.com)
-Last Modified: Tue Apr 10 22:28:44 CST 2018*/
-
 /*
-Given a binary tree, find its maximum depth.
+  Given a binary tree, find its maximum depth.
 
-        The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+  The maximum depth is the number of nodes along the longest path from the
+root node down to the farthest leaf node.
 
-        For example:
-        Given binary tree [3,9,20,null,null,15,7],
+For example:
+  Given binary tree [3,9,20,null,null,15,7],
 
-        3
+         3
         / \
-        9  20
-        /  \
-        15   7
-        return its depth = 3.*/
+       9  20
+       /  \
+      15   7
 
-
-import java.util.*;
-import java.lang.Math;
-import java.lang.System;
-import java.lang.Integer;
-
-
-public class Main {
-
-    public static void main(String[] args) throws ArithmeticException {
-
-        String input = "ab";
-
-        boolean answer = new Solution().repeatedSubstringPattern(input);
-
-        System.out.println("haha");
-    }
-
-}
-
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode(int x) {
-        val = x;
-    }
-}
+  return its depth = 3.
+*/
 
 
 class TreeNode {
+
     int val;
     TreeNode left;
     TreeNode right;
@@ -59,22 +30,14 @@ class TreeNode {
 
 
 class Solution {
+
     public int maxDepth(TreeNode root) {
 
         if (root == null) {
             return 0;
         }
 
-        if (root.left != null && root.right != null) {
-            return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-        } else {
-            if (root.left != null) {
-                return 1 + maxDepth(root.left);
-            } else if (root.right != null) {
-                return 1 + maxDepth(root.right);
-            } else {
-                return 1;
-            }
-        }
+        return 1 + Math.max(maxDepth(root.left),
+                maxDepth(root.right));
     }
 }
