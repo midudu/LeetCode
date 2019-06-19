f8b48d7f9d2f0b7b0966b36251330aa5e08066ad
ChenBo <chenbo.mddboc@bytedance.com>
2019-06-19 18:21:08 +0800

    review 766 of LeetCode

--- Java_Version/766_Toeplitz_Matrix.java
+++ Java_Version/766_Toeplitz_Matrix.java
@@ -1,91 +1,86 @@
-/*Author: Bochen (mddboc@foxmail.com)
-Last Modified: Tue Apr 10 22:28:45 CST 2018*/
+/*
+  A matrix is Toeplitz if every diagonal from top-left to bottom-right has the
+same element.
 
-/*A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+  Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
 
-        Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+Example 1:
 
-
-        Example 1:
-
-        Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+        Input:
+        matrix = [
+        [1,2,3,4],
+        [5,1,2,3],
+        [9,5,1,2]
+        ]
         Output: True
+
         Explanation:
-        1234
-        5123
-        9512
+        In the above grid, the diagonals are:
+        "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+        In each diagonal all elements are the same, so the answer is True.
 
-        In the above grid, the diagonals are "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]", and in each diagonal all elements are the same, so the answer is True.
-        Example 2:
+Example 2:
 
-        Input: matrix = [[1,2],[2,2]]
+        Input:
+        matrix = [
+        [1,2],
+        [2,2]
+        ]
         Output: False
+
         Explanation:
         The diagonal "[1, 2]" has different elements.
-        Note:
+
+Note:
 
         matrix will be a 2D array of integers.
         matrix will have a number of rows and columns in range [1, 20].
-        matrix[i][j] will be integers in range [0, 99].*/
+        matrix[i][j] will be integers in range [0, 99].
 
-import java.util.*;
-import java.lang.Math;
-import java.lang.System;
-import java.lang.Integer;
+Follow up:
 
+        What if the matrix is stored on disk, and the memory is limited such
+      that you can only load at most one row of the matrix into the memory at
+      once?
 
-public class Main {
-
-    public static void main(String[] args) {
-        String[] input = {"5", "2", "C", "D", "+"};
-
-        Solution solution = new Solution();
-        int receive = solution.calPoints(input);
-
-
-        System.out.println("haha");
-    }
-
-}
-
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode(int x) {
-        val = x;
-    }
-}
-
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
-}
-
+        What if the matrix is so large that you can only load up a partial row
+      into the memory at once?
+*/
 
 class Solution {
+
     public boolean isToeplitzMatrix(int[][] matrix) {
 
-        if (matrix == null || matrix.length <= 1) {
-            return true;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
         }
 
-        int rows = matrix.length;
-        int cols = matrix[0].length;
+        for (int row = 1; row < matrix.length; row++) {
 
-        for (int row = 0; row < rows - 1; row++) {
-            for (int col = 0; col < cols - 1; col++) {
+            if (!isToeplitzMatrixHelper(matrix, row, 0)) {
+                return false;
+            }
+        }
 
-                if ( matrix[row][col] != matrix[row+1][col+1]){
-                    return false;
-                }
+        for (int col = 0; col < matrix[0].length; col++) {
+
+            if (!isToeplitzMatrixHelper(matrix,0, col)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isToeplitzMatrixHelper(int[][] matrix, int startRow, int startCol) {
+
+        int value = matrix[startRow][startCol];
+
+        for (int row = startRow + 1, col = startCol + 1;
+             row < matrix.length && col < matrix[0].length; row++, col++) {
+
+            if (matrix[row][col] != value) {
+                return false;
             }
         }
 
