d226d1150088f0adc00b44d371a152f4c7334933
ChenBo <chenbo.mddboc@bytedance.com>
2019-06-30 14:10:22 +0800

    add 680 of LeetCode

--- Java_Version/680_Valid_Palindrome_II.java
+++ Java_Version/680_Valid_Palindrome_II.java
@@ -1,84 +1,62 @@
-/*Author: Bochen (mddboc@foxmail.com)
-Last Modified: Tue Apr 10 22:28:45 CST 2018*/
+/*
+  Given a non-empty string s, you may delete at most one character. Judge
+whether you can make it a palindrome.
 
-/*Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
-
-        Example 1:
+Example 1:
         Input: "aba"
         Output: True
-        Example 2:
+
+Example 2:
         Input: "abca"
         Output: True
+
         Explanation: You could delete the character 'c'.
-        Note:
-        The string will only contain lowercase characters a-z. The maximum length of the string is 50000.*/
 
-
-import java.util.*;
-import java.lang.Math;
-import java.lang.System;
-import java.lang.Integer;
-
-
-public class Main {
-
-    public static void main(String[] args) throws ArithmeticException {
-
-
-        int[][] inpur = {{1}};
-        new Solution().imageSmoother(inpur);
-
-        System.out.println("haha");
-    }
-
-}
-
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode(int x) {
-        val = x;
-    }
-}
-
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
-}
-
+Note:
+  The string will only contain lowercase characters a-z. The maximum length of
+the string is 50000.
+*/
 
 class Solution {
-    public boolean validPalindrome(String s) {
 
-        return validPalindromeHelper(s, 0, s.length() - 1, 0);
+    public static void main(String[] args) {
+
+        String s = "abcda";
+
+        System.out.println(new Solution().validPalindrome(s));
     }
 
-    private boolean validPalindromeHelper(String s,
-                                          int startPointer, int endPointer, int count) {
+    public boolean validPalindrome(String s) {
 
-        while (startPointer < endPointer) {
-            if (s.charAt(startPointer) != s.charAt(endPointer)) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
 
-                count++;
-                if (count == 2) {
-                    return false;
-                }
+        return validPalindromeHelper(s, 0, s.length() - 1);
+    }
 
-                return validPalindromeHelper(s, startPointer, endPointer - 1, count)
-                        || validPalindromeHelper(s, startPointer + 1, endPointer, count);
+    private boolean validPalindromeHelper(String s, int startIndex, int endIndex) {
 
-            } else {
-                startPointer++;
-                endPointer--;
+        if (startIndex >= endIndex) {
+            return true;
+        }
+
+        if (s.charAt(startIndex) == s.charAt(endIndex)) {
+            return validPalindromeHelper(s, startIndex + 1, endIndex - 1);
+        } else {
+            return ifStrictlyPalindrome(s, startIndex, endIndex - 1)
+                    || ifStrictlyPalindrome(s, startIndex + 1, endIndex);
+        }
+    }
+
+    private boolean ifStrictlyPalindrome(String s, int startIndex, int endIndex) {
+
+        while (startIndex < endIndex) {
+            if (s.charAt(startIndex) != s.charAt(endIndex)) {
+                return false;
             }
+            startIndex++;
+            endIndex--;
         }
 
         return true;
--- Java_Version/693_Binary_Number_with_Alternating_Bits.java
+++ Java_Version/693_Binary_Number_with_Alternating_Bits.java
@@ -1,92 +1,69 @@
-/*Author: Bochen (mddboc@foxmail.com)
-Last Modified: Tue Apr 10 22:28:45 CST 2018*/
+/*
+  Given a positive integer, check whether it has alternating bits: namely, if
+two adjacent bits will always have different values.
 
-/*Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
+Example 1:
 
-        Example 1:
         Input: 5
         Output: True
+
         Explanation:
-        The binary representation of 5 is: 101
-        Example 2:
+          The binary representation of 5 is: 101
+
+Example 2:
+
         Input: 7
         Output: False
+
         Explanation:
-        The binary representation of 7 is: 111.
-        Example 3:
+          The binary representation of 7 is: 111.
+
+Example 3:
+
         Input: 11
         Output: False
+
         Explanation:
         The binary representation of 11 is: 1011.
-        Example 4:
+
+Example 4:
+
         Input: 10
         Output: True
         Explanation:
-        The binary representation of 10 is: 1010.*/
-
-import java.util.*;
-import java.lang.Math;
-import java.lang.System;
-import java.lang.Integer;
-
-
-public class Main {
-
-    public static void main(String[] args) {
-        String[] input = {"5", "2", "C", "D", "+"};
-
-        Solution solution = new Solution();
-        int receive = solution.calPoints(input);
-
-
-        System.out.println("haha");
-    }
-
-}
-
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode(int x) {
-        val = x;
-    }
-}
-
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
-}
-
+        The binary representation of 10 is: 1010.
+*/
 
 class Solution {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        System.out.println(solution.hasAlternatingBits(5));
+        System.out.println(solution.hasAlternatingBits(7));
+        System.out.println(solution.hasAlternatingBits(11));
+        System.out.println(solution.hasAlternatingBits(10));
+    }
+
     public boolean hasAlternatingBits(int n) {
 
-        int mark = Integer.highestOneBit(n);
-        int lastBit = 0;
-        int temp;
+        if (n <= 0) {
+            throw new RuntimeException("illegal input");
+        }
 
-        while (mark != 0) {
-            temp = n & mark;
-            if (temp != 0) {
-                if (lastBit != 0){
-                    return false;
-                }
-                lastBit = 1;
-            } else {
-                if (lastBit == 0){
-                    return false;
-                }
-                lastBit = 0;
+        int lastBit = (n & 1);
+
+        while (n != 0) {
+
+            n >>= 1;
+            int currentBit = (n & 1);
+
+            if (currentBit == lastBit) {
+                return false;
             }
-            mark = mark >> 1;
+
+            lastBit = currentBit;
         }
 
         return true;
