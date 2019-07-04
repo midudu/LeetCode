073f8d4e81c03f083d6ed5293d85e8613c406f8f
ChenBo <chenbo.mddboc@bytedance.com>
2019-07-04 14:44:19 +0800

    add 667; review 669 of LeetCode

--- Java_Version/669_Trim_a_Binary_Search_Tree.java
+++ Java_Version/669_Trim_a_Binary_Search_Tree.java
@@ -1,71 +1,49 @@
-/*Author: Bochen (mddboc@foxmail.com)
-Last Modified: Tue Apr 10 22:28:45 CST 2018*/
+/*
+  Given a binary search tree and the lowest and highest boundaries as L and R,
+trim the tree so that all its elements lies in [L, R] (R >= L). You might need
+to change the root of the tree, so the result should return the new root of the
+trimmed binary search tree.
 
-/*Given a binary search tree and the lowest and highest boundaries as L and R, trim the tree so that all its elements lies in [L, R] (R >= L). You might need to change the root of the tree, so the result should return the new root of the trimmed binary search tree.
+Example 1:
 
-        Example 1:
         Input:
-        1
-        / \
-        0   2
+
+            1
+           / \
+          0   2
 
         L = 1
         R = 2
 
         Output:
-        1
-        \
-        2
-        Example 2:
+
+           1
+            \
+             2
+
+Example 2:
+
         Input:
-        3
-        / \
-        0   4
-        \
-        2
-        /
-        1
+
+             3
+            / \
+           0   4
+            \
+             2
+            /
+           1
 
         L = 1
         R = 3
 
         Output:
-        3
-        /
-        2
-        /
-        1*/
 
-import java.util.*;
-import java.lang.Math;
-import java.lang.System;
-import java.lang.Integer;
-
-
-public class Main {
-
-    public static void main(String[] args) {
-        int[] nums = {5, 5, 3, 5, 1, -5, 1, -2};
-
-        Solution solution = new Solution();
-        int receive = solution.findComplement(5);
-
-
-        System.out.println("haha");
-    }
-
-}
-
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode(int x) {
-        val = x;
-    }
-}
-
+            3
+           /
+          2
+         /
+        1
+*/
 
 class TreeNode {
     int val;
@@ -79,24 +57,43 @@ class TreeNode {
 
 
 class Solution {
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(0);
+        root.right = new TreeNode(4);
+        root.left.right = new TreeNode(2);
+        root.left.right.left = new TreeNode(1);
+
+        TreeNode newRoot = new Solution().trimBST(root, 1, 3);
+
+        System.out.println();
+    }
+
     public TreeNode trimBST(TreeNode root, int L, int R) {
 
         if (root == null) {
             return null;
         }
 
-        if (root.val > R) {
+        return trimBSTHelper(root, L, R);
+    }
 
-            return trimBST(root.left, L, R);
-        } else if (root.val < L) {
+    private TreeNode trimBSTHelper(TreeNode root, int L, int R) {
 
-            return trimBST(root.right, L, R);
+        if (root == null) {
+            return null;
         }
 
-        TreeNode resultTreeNode =  new TreeNode(root.val);
-        resultTreeNode.left = trimBST(root.left, L, R);
-        resultTreeNode.right = trimBST(root.right, L, R);
-
-        return resultTreeNode;
+        if (root.val < L) {
+            return trimBSTHelper(root.right, L, R);
+        } else if (root.val > R) {
+            return trimBSTHelper(root.left, L, R);
+        } else {
+            root.left = trimBSTHelper(root.left, L, R);
+            root.right = trimBSTHelper(root.right, L, R);
+            return root;
+        }
     }
 }
