15aa30ee3edfbdb75a7a8f3d6880911fc7b957e1
Bo Chen <412862016@qq.com>
2019-07-05 21:22:07 +0800

    temp

--- Java_Version/TestJava/src/Solution.java
+++ Java_Version/TestJava/src/Solution.java
@@ -1,95 +1,102 @@
 /*
-  In this problem, a tree is an undirected graph that is connected and has no
-cycles.
+  Implement a MapSum class with insert, and sum methods.
 
-  The given input is a graph that started as a tree with N nodes (with distinct
-values 1, 2, ..., N), with one additional edge added. The added edge has two
-different vertices chosen from 1 to N, and was not an edge that already existed.
+  For the method insert, you'll be given a pair of (string, integer). The
+string represents the key and the integer represents the value. If the key
+already existed, then the original key-value pair will be overridden to the new
+one.
 
-  The resulting graph is given as a 2D-array of edges. Each element of edges is
-a pair [u, v] with u < v, that represents an undirected edge connecting nodes u
-and v.
+  For the method sum, you'll be given a string representing the prefix, and you
+need to return the sum of all the pairs' value whose key starts with the prefix.
 
-  Return an edge that can be removed so that the resulting graph is a tree of N
-nodes. If there are multiple answers, return the answer that occurs last in the
-given 2D-array. The answer edge [u, v] should be in the same format, with u < v.
+  Example 1:
 
-Example 1:
-
-        Input: [[1,2], [1,3], [2,3]]
-        Output: [2,3]
-        Explanation: The given undirected graph will be like this:
-
-        1
-        / \
-        2 - 3
-
-Example 2:
-        Input: [[1,2], [2,3], [3,4], [1,4], [1,5]]
-        Output: [1,4]
-        Explanation: The given undirected graph will be like this:
-        5 - 1 - 2
-        |   |
-        4 - 3
-
-Note:
-  The size of the input 2D-array will be between 3 and 1000.
-
-  Every integer represented in the 2D-array will be between 1 and N, where N is
-the size of the input array.
+        Input: insert("apple", 3), Output: Null
+        Input: sum("ap"), Output: 3
+        Input: insert("app", 2), Output: Null
+        Input: sum("ap"), Output: 5
 */
 
-import java.util.Arrays;
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum obj = new MapSum();
+ * obj.insert(key,val);
+ * int param_2 = obj.sum(prefix);
+ */
 
-class Solution {
+class Node {
+
+    Node[] children = new Node[26];
+    int value;
+    String string;
+}
+
+
+class MapSum {
 
     public static void main(String[] args) {
 
-        int[][] edges = {{1,2}, {2,3}, {3,4}, {1,4}, {1,5}};
+        MapSum mapSum = new MapSum();
 
-        int[] result = new Solution().findRedundantConnection(edges);
+        mapSum.insert("apple", 3);
+        System.out.println(mapSum.sum("ap"));
 
-        System.out.println(Arrays.toString(result));
+        mapSum.insert("app", 2);
+        System.out.println(mapSum.sum("ap"));
     }
 
-    public int[] findRedundantConnection(int[][] edges) {
+    private Node root = new Node();
 
-        int[] result = new int[2];
+    /** Initialize your data structure here. */
+    public MapSum() {
+    }
 
-        if (edges == null || edges.length == 0) {
-            return result;
+    public void insert(String key, int val) {
+
+        if (key == null || key.length() == 0) {
+            return;
         }
 
-        int[] disjointSet = new int[edges.length + 1];
+        Node currentNode = this.root;
 
-        for (int i = 0; i < edges.length; i++) {
+        for (int i = 0; i < key.length(); i++) {
 
-            int root1 = find(disjointSet, edges[i][0]);
-            int root2 = find(disjointSet, edges[i][1]);
+            char currentChar = key.charAt(i);
 
-            if (root1 == root2) {
-                result[0] = edges[i][0];
-                result[1] = edges[i][1];
-                break;
+            if (currentNode.children[currentChar - 'a'] == null) {
+                currentNode.children[currentChar - 'a'] = new Node();
             }
 
-            union(disjointSet, root1, root2);
+            currentNode = currentNode.children[currentChar - 'a'];
+            currentNode
+        }
+
+        currentNode.string = key;
+        currentNode.value = val;
+    }
+
+    public int sum(String prefix) {
+
+        if (prefix == null || prefix.length() == 0) {
+            return 0;
+        }
+
+        int result = 0;
+        Node currentNode = this.root;
+
+        for (int i = 0; i < prefix.length(); i++) {
+
+            Node nextNode = currentNode.children[prefix.charAt(i) - 'a'];
+            if (nextNode == null) {
+                break;
+            }
+            result += nextNode.value;
+
+            currentNode = nextNode;
         }
 
         return result;
     }
-
-    private int find(int[] disjointSet, int node) {
-
-        while (disjointSet[node] != 0) {
-            node = disjointSet[node];
-        }
-
-        return node;
-    }
-
-    private void union(int[] disjointSet, int node1, int node2) {
-
-        disjointSet[node1] = node2;
-    }
 }
+
+
