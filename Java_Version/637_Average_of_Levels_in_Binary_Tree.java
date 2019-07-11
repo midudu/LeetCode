82cbdf75a7344cd06c970dff2f4edc47310c3486
ChenBo <chenbo.mddboc@bytedance.com>
2019-07-11 15:46:22 +0800

    add 633, 637 of LeetCode

--- Java_Version/637_Average_of_Levels_in_Binary_Tree.java
+++ Java_Version/637_Average_of_Levels_in_Binary_Tree.java
@@ -1,52 +1,32 @@
-/*Author: Bochen (mddboc@foxmail.com)
-Last Modified: Tue Apr 10 22:28:45 CST 2018*/
+/*
+  Given a non-empty binary tree, return the average value of the nodes on each
+level in the form of an array.
 
-/*Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
-        Example 1:
+Example 1:
+
         Input:
-        3
-        / \
-        9  20
-        /  \
-        15   7
+
+               3
+              / \
+             9  20
+           /  \
+          15   7
+
         Output: [3, 14.5, 11]
-        Explanation:
-        The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
-        Note:
-        The range of node's value is in the range of 32-bit signed integer.*/
 
+Explanation:
 
-import java.util.*;
-import java.lang.Math;
-import java.lang.System;
-import java.lang.Integer;
+  The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level
+2 is 11. Hence return [3, 14.5, 11].
 
+Note:
+  The range of node's value is in the range of 32-bit signed integer.
+*/
 
-public class Main {
-
-    public static void main(String[] args) throws ArithmeticException {
-
-        int a = 3, b = 0;
-
-
-        int result = a / b;
-
-
-        System.out.println("haha");
-    }
-
-}
-
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode(int x) {
-        val = x;
-    }
-}
-
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 class TreeNode {
     int val;
@@ -61,34 +41,47 @@ class TreeNode {
 
 class Solution {
 
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.left.left = new TreeNode(15);
+        root.left.right = new TreeNode(7);
+
+        List<Double> result = new Solution().averageOfLevels(root);
+
+        System.out.println(result.toString());
+    }
+
     public List<Double> averageOfLevels(TreeNode root) {
 
         List<Double> result = new ArrayList<>();
-
         if (root == null) {
             return result;
         }
 
         Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
+        queue.offer(root);
 
         while (!queue.isEmpty()) {
+
             int size = queue.size();
-            double sum = 0.0;
+            double sum = 0;
 
             for (int i = 0; i < size; i++) {
-                TreeNode tempNode = queue.poll();
-                sum += tempNode.val;
-                if (tempNode.left != null) {
-                    queue.add(tempNode.left);
+                TreeNode currentNode = queue.poll();
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
                 }
-                if (tempNode.right != null) {
-                    queue.add(tempNode.right);
-                    queue.add()
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
                 }
+
+                sum += currentNode.val;
             }
 
-            result.add(sum/size);
+            result.add(sum / size);
         }
 
         return result;
