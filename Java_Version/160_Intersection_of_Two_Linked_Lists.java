67b025ee3297f16bcbbe6aed6eeeee9ba57136d6
ChenBo <chenbo.mddboc@bytedance.com>
2019-08-16 18:52:36 +0800

    review several

--- Java_Version/160_Intersection_of_Two_Linked_Lists.java
+++ Java_Version/160_Intersection_of_Two_Linked_Lists.java
@@ -1,131 +1,68 @@
-/*Author: Bochen (mddboc@foxmail.com)
-Last Modified: Tue Apr 10 22:28:44 CST 2018*/
+/*
+  Write a program to find the node at which the intersection of two singly
+linked lists begins.
 
-/* Write a program to find the node at which the intersection of two singly linked lists begins.
+  For example, the following two linked lists:
 
 
-For example, the following two linked lists:
+    A:          a1 → a2
+                       ↘
+                         c1 → c2 → c3
+                       ↗
+    B:     b1 → b2 → b3
 
-A:          a1 → a2
-                   ↘
-                     c1 → c2 → c3
-                   ↗            
-B:     b1 → b2 → b3
-begin to intersect at node c1.
+  begin to intersect at node c1.
 
 
 Notes:
-
-If the two linked lists have no intersection at all, return null.
-The linked lists must retain their original structure after the function returns.
-You may assume there are no cycles anywhere in the entire linked structure.
-Your code should preferably run in O(n) time and use only O(1) memory. */
-
-
-import java.util.*;
-import java.lang.Math;
-import java.lang.System;
-import java.lang.Integer;
-
-
-public class Main {
-
-    public static void main(String[] args) throws ArithmeticException {
-
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(2);
-        root.left.left = new TreeNode(3);
-        root.left.right = new TreeNode(4);
-        root.right.left = new TreeNode(4);
-        root.right.right = new TreeNode(3);
-
-        boolean result = new Solution().isSymmetric(root);
-
-        System.out.println(result);
-    }
-
-}
-
+  1. If the two linked lists have no intersection at all, return null.
+  2. The linked lists must retain their original structure after the function returns.
+  3. You may assume there are no cycles anywhere in the entire linked structure.
+  4. Your code should preferably run in O(n) time and use only O(1) memory.
+*/
 
 class ListNode {
-    int val;
-    ListNode next;
+	int val;
+	ListNode next;
 
-    ListNode(int x) {
-        val = x;
-    }
+	ListNode(int x) {
+		val = x;
+	}
 }
-
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
-}
-
 
 class Solution {
-    
-	//version 2: better one
+
 	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-		
-		int aLength = 0, bLength = 0;
-		ListNode aPointer = headA, bPointer = headB;
-		
-		while ( aPointer != null ) {
-			aLength++;
-			aPointer = aPointer.next;
+
+		if (headA == null || headB == null) {
+			return null;
 		}
-		while ( bPointer != null ) {
-			bLength++;
-			bPointer = bPointer.next;
+
+		int lengthA = getLength(headA);
+		int lengthB = getLength(headB);
+
+		ListNode pointer1 = (lengthA > lengthB ? headA : headB);
+		for (int i = 0; i < Math.abs(lengthA - lengthB); i++) {
+			pointer1 = pointer1.next;
 		}
-		
-		if ( aLength < bLength ) {
-            return getIntersectionNodeHelper(headA, headB, bLength - aLength);	
-		} else {
-			return getIntersectionNodeHelper(headB, headA, aLength - bLength);
+
+		ListNode pointer2 = (lengthA > lengthB ? headB : headA);
+		while (pointer1 != pointer2) {
+			pointer1 = pointer1.next;
+			pointer2 = pointer2.next;
 		}
-		
+
+		return pointer1;
 	}
-	
-	private ListNode getIntersectionNodeHelper(ListNode headA, ListNode headB, int deltaLength) {
-		
-	    for (int i = deltaLength; i > 0; i-- ) {
-			
-			headB = headB.next;
+
+	private int getLength(ListNode head) {
+
+		int result = 0;
+		while (head != null) {
+			result++;
+			head = head.next;
 		}
-		
-		while (true) {
-			
-			if ( headA == headB ) {
-				return headA;
-			}
-			
-			headA = headA.next;
-			headB = headB.next;
-		}
+
+		return result;
 	}
-	
-	
-	//version 1
-	/* public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-
-        ListNode nodeA = headA, nodeB = headB;
-
-        while (true) {
-
-            if (nodeA == nodeB) {
-                return nodeA;
-            }
-
-            nodeA = (nodeA == null) ? headB : nodeA.next;
-            nodeB = (nodeB == null) ? headA : nodeB.next;
-        }
-    } */
 }
--- Java_Version/169_Majority_Element.java
+++ Java_Version/169_Majority_Element.java
@@ -1,47 +1,37 @@
-/*Author: Bochen (mddboc@foxmail.com)
-Last Modified: Tue Apr 10 22:28:44 CST 2018*/
+/*
+  Given an array of size n, find the majority element. The majority element is
+the element that appears more than ⌊ n/2 ⌋ times.
 
-/*Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+  You may assume that the array is non-empty and the majority element always
+exist in the array.
+*/
 
-        You may assume that the array is non-empty and the majority element always exist in the array.*/
-
-import java.util.Arrays;
-import java.lang.Math;
-import java.lang.System;
-import java.util.HashMap;
-import java.lang.Integer;
-
-// Method 1: sort
-/* class Solution {
-    public int majorityElement(int[] nums) {
-
-        Arrays.sort(nums);
-
-        return nums[nums.length/2];
-    }
-} */
-
-// Method 2: The Boyer-Moore algorithm
 class Solution {
 
-    public int majorityElement(int[] nums) {
+	public int majorityElement(int[] nums) {
 
-        int majority = nums[0];
-        int count = 0;
-		
-		for (int vote: nums) {
-			
-			if ( vote == majority ) {
+		if (nums == null || nums.length == 0) {
+			throw new RuntimeException("illegal input");
+		}
+
+		int majority = nums[0];
+		int count = 1;
+
+		for (int i = 1; i < nums.length; i++) {
+
+			if (nums[i] == majority) {
 				count++;
-			} else if (count == 0) {
-				majority = vote;
+				continue;
+			}
+
+			if (count == 0) {
+				majority = nums[i];
 				count = 1;
 			} else {
 				count--;
 			}
 		}
-		
-		// Because "You may assume that the array is non-empty and the majority element always exist in the array"
+
 		return majority;
-    }
+	}
 }
