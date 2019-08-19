6f1a488e4e2fc51ff4fe34b4423063ed3f5b7c34
ChenBo <chenbo.mddboc@bytedance.com>
2019-08-19 14:24:42 +0800

    review 200, 204, 205

--- Java_Version/200_Number_of_Islands.java
+++ Java_Version/200_Number_of_Islands.java
@@ -1,13 +1,15 @@
 /*
-Given a 2d grid map of '1's (land) and '0's (water),
-count the number of islands.
-An island is surrounded by water and is formed
-by connecting adjacent lands horizontally or vertically.
-You may assume all four edges of the grid are all surrounded by water.
+  Given a 2d grid map of '1's (land) and '0's (water), count the number of
+islands.
 
-        Example 1:
+  An island is surrounded by water and is formed by connecting adjacent lands
+horizontally or vertically. You may assume all four edges of the grid are all
+surrounded by water.
+
+Example 1:
 
         Input:
+
         11110
         11010
         11000
@@ -15,7 +17,7 @@ You may assume all four edges of the grid are all surrounded by water.
 
         Output: 1
 
-        Example 2:
+Example 2:
 
         Input:
         11000
@@ -23,15 +25,14 @@ You may assume all four edges of the grid are all surrounded by water.
         00100
         00011
 
-        Output: 3*/
+        Output: 3
+*/
 
 class Solution {
 
     public int numIslands(char[][] grid) {
 
-        if (grid == null
-                || grid.length == 0 || grid[0].length == 0) {
-
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
             return 0;
         }
 
@@ -45,6 +46,7 @@ class Solution {
                 if (hasVisited[row][col] || grid[row][col] == '0') {
                     continue;
                 }
+
                 result++;
 
                 numIslandsHelper(grid, row, col, hasVisited);
