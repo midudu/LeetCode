1877a9f738241fe85bc763b780933969f7a2b107
Bo Chen <412862016@qq.com>
2019-08-24 11:45:24 +0800

    add several

--- Java_Version/172_Factorial_Trailing_Zeroes.java
+++ Java_Version/172_Factorial_Trailing_Zeroes.java
@@ -1,40 +1,19 @@
-/*Author: Bochen (mddboc@foxmail.com)
-Last Modified: Tue Apr 10 22:28:44 CST 2018*/
+/*
+  Given an integer n, return the number of trailing zeroes in n!.
 
-/*Given an integer n, return the number of trailing zeroes in n!.
-
-        Note: Your solution should be in logarithmic time complexity.*/
-
-
-import java.util.*;
-
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
-}
-
-public class Test {
-    public static void main(String[] args) {
-
-        int num = 1808548329;
-
-        new Solution().trailingZeroes(num);
-    }
-}
+Note: 
+  Your solution should be in logarithmic time complexity.
+*/
 
 
 class Solution {
+    
     public int trailingZeroes(int n) {
 
         if (n == 0) {
             return 0;
         }
+        
         return (n / 5) + trailingZeroes(n / 5);
     }
 }
--- Java_Version/198_House_Robber.java
+++ Java_Version/198_House_Robber.java
@@ -1,56 +1,37 @@
-/*Author: Bochen (mddboc@foxmail.com)
-Last Modified: Tue Apr 10 22:28:44 CST 2018*/
+/*
+  You are a professional robber planning to rob houses along a street. Each
+house has a certain amount of money stashed, the only constraint stopping you
+from robbing each of them is that adjacent houses have security system
+connected and it will automatically contact the police if two adjacent houses
+were broken into on the same night.
 
-/*You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
-
-        Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.*/
-
-import java.util.*;
-
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
-}
-
-public class Test {
-    public static void main(String[] args) {
-
-        int num = 2147483648;
-
-        new Solution().reverseBits(num);
-    }
-}
-
+  Given a list of non-negative integers representing the amount of money of
+each house, determine the maximum amount of money you can rob tonight without
+alerting the police.
+*/
 
 class Solution {
+
+    // f(x) = max(f(x-1), f(x-2) + nums[x])
     public int rob(int[] nums) {
 
         if (nums == null || nums.length == 0) {
             return 0;
-        } else if (nums.length == 1) {
+        }
+        if (nums.length == 1) {
             return nums[0];
-        } else if (nums.length == 2) {
-            return Math.max(nums[0], nums[1]);
         }
 
-        int numsLength = nums.length;
+        int lastTwoResult = nums[0];
+        int lastResult = Math.max(nums[0], nums[1]);
 
-        int[] storage = new int[numsLength];
-        storage[0] = nums[0];
-        storage[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
 
-        for (int i = 2; i < numsLength; i++) {
-
-            int temp = storage[i - 2] + nums[i];
-            storage[i] = Math.max(temp, storage[i - 1]);
+            int currentResult = Math.max(lastResult, lastTwoResult + nums[i]);
+            lastTwoResult = lastResult;
+            lastResult = currentResult;
         }
 
-        return storage[numsLength - 1];
+        return lastResult;
     }
 }
